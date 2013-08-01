addc1adf73b440dd2718d71bade454c9af63fe84
guybedford <guybedford@gmail.com>
2013-08-01 15:57:39 -0700

    shim adjustment

--- select2.js
+++ select2.js
@@ -1,2 +1,2 @@
 require('ivaynberg/select2@3.4/select2!css');
-module.exports = require('ivaynberg/select2@3.4/select2 | http://code.jquery.com/jquery-1.10.1.min.js');
+module.exports = require('github:ivaynberg/select2@3.4/select2 | http://code.jquery.com/jquery-1.10.1.min.js');
