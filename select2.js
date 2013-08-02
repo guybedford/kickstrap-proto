3342b27b33732707b2c0677f37572ac6659b175b
guybedford <guybedford@gmail.com>
2013-08-02 12:35:54 -0700

    exact version for shim

--- select2.js
+++ select2.js
@@ -1,2 +1,2 @@
 require('ivaynberg/select2@3.4/select2!css');
-module.exports = require('github:ivaynberg/select2@3.4/select2 | jspm/registry/jquery');
+module.exports = require('github:ivaynberg/select2@3.4.1/select2 | jspm/registry/jquery');
