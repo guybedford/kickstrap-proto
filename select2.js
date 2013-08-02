0b6440b33d1d428a3d2ef346fddc45e7ec921d43
guybedford <guybedford@gmail.com>
2013-08-02 12:55:30 -0700

    registry adjustment

--- select2.js
+++ select2.js
@@ -1,2 +1,2 @@
-require('ivaynberg/select2@3.4.1/select2!css');
+require('github:ivaynberg/select2@3.4.1/select2!css');
 module.exports = require('github:ivaynberg/select2@3.4.1/select2 | jspm:jquery');
